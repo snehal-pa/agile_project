3d9a7e292d82055f761e792836f87e29ea38558c
snehal patel <snehal.kathiriya@gmail.com>
2020-11-30 15:15:43 +0100

    first commit

---
+++ agile_project_backend/src/main/java/com/agile_project/AgileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.agile_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileProjectApplication.class, args);
+	}
+
+}
---
+++ agile_project_backend/src/test/java/com/agile_project/AgileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
